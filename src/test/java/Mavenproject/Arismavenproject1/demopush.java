5be1e381c116fdfdfe96bde9f7e6bd72b1fa2426
00004946 <00004946@AGI-LT-1137.arisglobal.com>
2021-09-24 19:56:02 +0530

    helloframe

---
+++ src/test/java/Mavenproject/Arismavenproject1/demopush.java
@@ -0,0 +1,10 @@
+package Mavenproject.Arismavenproject1;
+
+public class demopush {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello");
+	}
+
+}
